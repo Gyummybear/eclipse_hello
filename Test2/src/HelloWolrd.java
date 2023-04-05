d99cf598bb9198d929cdf2784299a3230146e374
ikjm31 <ikmj31@gmail.com>
2023-04-05 09:23:56 +0900

    asas

--- Test2/src/HelloWolrd.java
+++ Test2/src/HelloWolrd.java
@@ -4,11 +4,8 @@ public class HelloWolrd {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello his world");
-<<<<<<< HEAD
 		System.out.println("Hello your World");
-=======
 		System.out.println("Hello her world");
->>>>>>> refs/heads/hotifx
 	}
 
 }
